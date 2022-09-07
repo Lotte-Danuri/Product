234288d685f5ac4d2395c0d398e8ee76eb85b608
jinwooooooo <ko440338@gmail.com>
2022-09-07 19:50:52 +0900

    (FEAN-64) Feat : 초기 프로젝트 세팅

---
+++ src/test/java/com/lotte/danuri/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotte.danuri.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
