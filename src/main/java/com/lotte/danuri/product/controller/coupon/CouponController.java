2b4f6ae43c4226d6c7a606d05a033a326b3214ca
jinwooooooo <ko440338@gmail.com>
2022-09-14 17:54:24 +0900

    Refactor : Controller, Service, Dto, Repository 생성 - 쿠폰, 일반 사용자 상품 목록 조회/상세조회

---
+++ src/main/java/com/lotte/danuri/product/controller/coupon/CouponController.java
@@ -0,0 +1,14 @@
+package com.lotte.danuri.product.controller.coupon;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/admin/coupons")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class CouponController {
+
+}
