42a9cc360c590912addec18785510d8d61308518
jinwooooooo <ko440338@gmail.com>
2022-09-14 20:38:48 +0900

    (FEAN-39):(일반사용자) 상품 목록 조회/상세조회, 에러처리

--- src/main/java/com/lotte/danuri/product/controller/buyer/BuyerProductController.java
+++ src/main/java/com/lotte/danuri/product/controller/buyer/BuyerProductController.java
@@ -1,5 +1,48 @@
 package com.lotte.danuri.product.controller.buyer;
 
-public class BuyerProductController {
+import com.lotte.danuri.product.model.dto.ProductDto;
+import com.lotte.danuri.product.model.entity.Product;
+import com.lotte.danuri.product.model.response.ProductResponse;
+import com.lotte.danuri.product.service.buyer.BuyerProductService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/products")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class BuyerProductController {
+    private final BuyerProductService buyerProductService;
+
+    @GetMapping("")
+    public ResponseEntity<List<ProductResponse>> getProducts(){
+
+        Iterable<Product> productList = buyerProductService.getAllProducts();
+
+        List<ProductResponse> result = new ArrayList<>();
+        productList.forEach(v -> {
+            result.add(new ModelMapper().map(v, ProductResponse.class));
+        });
+
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<ProductResponse> getProduct(@PathVariable("productId") Long productId){
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        ProductDto productDto = buyerProductService.getProduct(productId);
+
+        ProductResponse productResponse = mapper.map(productDto, ProductResponse.class);
+
+        return ResponseEntity.ok(productResponse);
+    }
 }
--- src/main/java/com/lotte/danuri/product/service/buyer/BuyerProductServiceImpl.java
+++ src/main/java/com/lotte/danuri/product/service/buyer/BuyerProductServiceImpl.java
@@ -1,5 +1,40 @@
 package com.lotte.danuri.product.service.buyer;
 
-public class BuyerProductServiceImpl implements BuyerProductService{
+import com.lotte.danuri.product.error.ErrorCode;
+import com.lotte.danuri.product.exception.ProductNotFoundException;
+import com.lotte.danuri.product.model.dto.ProductDto;
+import com.lotte.danuri.product.model.entity.Product;
+import com.lotte.danuri.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class BuyerProductServiceImpl implements BuyerProductService{
+    private final ProductRepository productRepository;
+    public Iterable<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
+
+    public ProductDto getProduct(Long productId){
+        Optional<Product> optionalProduct = productRepository.findById(productId);
+        if (!optionalProduct.isPresent()){
+            throw new ProductNotFoundException("Product not present in the database", ErrorCode.PRODUCT_NOT_FOUND);
+        }
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        ProductDto productDto = mapper.map(optionalProduct.get(), ProductDto.class);
+
+        productDto.setCategoryFirstId(optionalProduct.get().getCategoryFirst().getId());
+        productDto.setCategorySecondId(optionalProduct.get().getCategorySecond().getId());
+        productDto.setCategoryThirdId(optionalProduct.get().getCategoryThird().getId());
+
+        return productDto;
+    }
 }
