83766fe410ce627201c1ad97341b4f85022167d3
jinwooooooo <ko440338@gmail.com>
2022-09-19 14:18:40 +0900

    (FEAN-53) Refactor : 예외 처리 추가

--- src/main/java/com/lotte/danuri/product/service/buyer/BuyerProductServiceImpl.java
+++ src/main/java/com/lotte/danuri/product/service/buyer/BuyerProductServiceImpl.java
@@ -2,6 +2,7 @@ package com.lotte.danuri.product.service.buyer;
 
 import com.lotte.danuri.product.error.ErrorCode;
 import com.lotte.danuri.product.exception.ProductNotFoundException;
+import com.lotte.danuri.product.exception.ProductWasDeletedException;
 import com.lotte.danuri.product.model.dto.ProductDto;
 import com.lotte.danuri.product.model.entity.CategoryFirst;
 import com.lotte.danuri.product.model.entity.CategorySecond;
@@ -25,23 +26,11 @@ public class BuyerProductServiceImpl implements BuyerProductService{
 
     private final ProductRepository productRepository;
     public List<ProductDto> getProducts(){
-        List<Product> products = productRepository.findAll();
-
+        List<Product> products = productRepository.findAllByDeletedDateIsNull();
         List<ProductDto> result = new ArrayList<>();
 
         products.forEach(v -> {
-            ProductDto productDto = ProductDto.builder()
-                    .id(v.getId())
-                    .productName(v.getProductName())
-                    .thumbnailUrl(v.getThumbnailUrl())
-                    .price(v.getPrice())
-                    .stock(v.getStock())
-                    .storeId(v.getStoreId())
-                    .likeCount(v.getLikeCount())
-                    .categoryFirstId(v.getCategoryFirst().getId())
-                    .categorySecondId(v.getCategorySecond().getId())
-                    .categoryThirdId(v.getCategoryThird().getId())
-                    .build();
+            ProductDto productDto = new ProductDto(v);
             result.add(productDto);
         });
         return result;
@@ -49,9 +38,18 @@ public class BuyerProductServiceImpl implements BuyerProductService{
 
     public ProductDto getProduct(Long productId){
         Optional<Product> product = productRepository.findById(productId);
+
+        // 예외 처리
+        // 1. 상품이 DB에 존재하지 않을 경우
         if(product.isEmpty()){
             throw new ProductNotFoundException("Product not present in the database", ErrorCode.PRODUCT_NOT_FOUND);
         }
+
+        // 2. 상품이 삭제된 경우
+        if(product.get().getDeletedDate() != null){
+            throw new ProductWasDeletedException("Product was deleted in the database", ErrorCode.PRODUCT_WAS_DELETED);
+        }
+
         ProductDto productDto = new ProductDto(product.get());
         return productDto;
     }
