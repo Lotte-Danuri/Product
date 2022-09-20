6c54978a838b27cd10a2a7e6026ae8c02159b3b4
jinwooooooo <ko440338@gmail.com>
2022-09-20 20:15:39 +0900

    (FEAN-53) Refactor : Swagger 적용

--- src/main/java/com/lotte/danuri/product/controller/buyer/BuyerProductController.java
+++ src/main/java/com/lotte/danuri/product/controller/buyer/BuyerProductController.java
@@ -3,6 +3,7 @@ package com.lotte.danuri.product.controller.buyer;
 import com.lotte.danuri.product.model.dto.ProductDto;
 import com.lotte.danuri.product.model.entity.Product;
 import com.lotte.danuri.product.service.buyer.BuyerProductService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
@@ -21,6 +22,7 @@ public class BuyerProductController {
     private final BuyerProductService buyerProductService;
 
     @GetMapping(value = "", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ApiOperation(value = "상품 조회", notes = "모든 상품을 조회한다.")
     public ResponseEntity<?> getProducts(){
 
         List<ProductDto> productList = buyerProductService.getProducts();
@@ -28,6 +30,7 @@ public class BuyerProductController {
     }
 
     @GetMapping(value = "/{productId}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ApiOperation(value = "상품 상세 조회", notes = "하나의 상품을 상세 조회한다.")
     public ResponseEntity<?> getProduct(@PathVariable("productId") Long productId){
 
         ProductDto productDto = buyerProductService.getProduct(productId);
