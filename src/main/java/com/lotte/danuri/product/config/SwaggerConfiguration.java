e18b528d610630a7d8c3088a42b54bb00f430711
jinwooooooo <ko440338@gmail.com>
2022-10-01 16:25:32 +0900

    (FEAN-112) - feat : Swagger 적용

--- src/main/java/com/lotte/danuri/product/config/SwaggerConfiguration.java
+++ src/main/java/com/lotte/danuri/product/config/SwaggerConfiguration.java
@@ -6,14 +6,20 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Server;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfiguration {
     @Bean
     public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2)
+        Server local = new Server("local", "http://localhost:8080", "for local usages", Collections.emptyList(), Collections.emptyList());
+        Server aws = new Server("aws", "http://http://43.201.18.146/product", "for aws", Collections.emptyList(), Collections.emptyList());
+
+        return new Docket(DocumentationType.OAS_30)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.lotte.danuri.product.controller"))
                 .paths(PathSelectors.any())
