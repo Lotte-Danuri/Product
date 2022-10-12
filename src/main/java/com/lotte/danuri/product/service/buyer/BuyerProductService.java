889d71fd8cf20338ad1e01a23e3d0d97371c5777
jinwooooooo <ko440338@gmail.com>
2022-10-12 16:26:26 +0900

    (FEAN-150) - feat : 조건(카테고리, 최소가격~최대가격, 브랜드)에 따른 상품 리스트 조회 api 구현

--- src/main/java/com/lotte/danuri/product/service/buyer/BuyerProductService.java
+++ src/main/java/com/lotte/danuri/product/service/buyer/BuyerProductService.java
@@ -1,6 +1,7 @@
 package com.lotte.danuri.product.service.buyer;
 
 import com.lotte.danuri.product.model.dto.ProductDto;
+import com.lotte.danuri.product.model.dto.request.ProductByConditionDto;
 import com.lotte.danuri.product.model.dto.response.ProductDetailResponseDto;
 import com.lotte.danuri.product.model.entity.Product;
 
@@ -10,4 +11,6 @@ public interface BuyerProductService {
     List<ProductDto> getProducts();
 
     ProductDetailResponseDto getProduct(Long productId);
+
+    List<ProductDto> getProductsByCondition(ProductByConditionDto productByConditionDto);
 }
