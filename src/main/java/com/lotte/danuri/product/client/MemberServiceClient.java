abb87e234202debe939b9c8f4ad0cf7007195334
jinwooooooo <ko440338@gmail.com>
2022-10-17 21:42:43 +0900

    (FEAN-167) - feat : Member API 호출 - storeId(LONG)를 받아 storeName(STRING) 리턴하는 API(GET) - brandId(LONG)를 받아 storeId(LIST<LONG>)를 리턴하는 API(GET)

--- src/main/java/com/lotte/danuri/product/client/MemberServiceClient.java
+++ src/main/java/com/lotte/danuri/product/client/MemberServiceClient.java
@@ -1,8 +1,17 @@
 package com.lotte.danuri.product.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @FeignClient(name = "member")
 public interface MemberServiceClient {
 
+    @GetMapping("/store/name/{storeId}")
+    String getNames(@PathVariable Long storeId);
+
+    @GetMapping("/store/{brandId}")
+    List<Long> getStoreId(@PathVariable Long brandId);
 }
