c436bc9f5802fb9fc4c429fe150646fad353f88e
jinwooooooo <ko440338@gmail.com>
2022-10-27 19:09:03 +0900

    (FEAN-200) feat : Recommend, Order 서비스 FeignClient

---
+++ src/main/java/com/lotte/danuri/product/client/OrderServiceClient.java
@@ -0,0 +1,14 @@
+package com.lotte.danuri.product.client;
+
+import com.lotte.danuri.product.model.dto.request.ProductListDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient(name = "order")
+public interface OrderServiceClient {
+    @PostMapping("/orders/pays/list/count")
+    List<Long> getOrdersCount (@RequestBody ProductListDto productListDto);
+}
