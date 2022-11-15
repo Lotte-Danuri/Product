4da8bad7927d763d29a1b6daf69e100ad9174bea
jinwooooooo <54875278+jinwooooooo@users.noreply.github.com>
2022-11-15 15:27:08 +0900

    (develop) fix : 쿠폰 등록 return 수정

--- src/main/java/com/lotte/danuri/product/controller/coupon/CouponController.java
+++ src/main/java/com/lotte/danuri/product/controller/coupon/CouponController.java
@@ -29,7 +29,7 @@ public class CouponController {
     public ResponseEntity createCoupon (@RequestBody CouponDto couponDto){
 
         Long couponId = couponService.createCoupon(couponDto);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(couponId);
     }
 
     @GetMapping(value = "", produces = {MediaType.APPLICATION_JSON_VALUE})
